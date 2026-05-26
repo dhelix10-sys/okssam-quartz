08af443da795a5d4affd513cb3717a10e274988c
dhelix10-sys <dhelix10-sys@users.noreply.github.com>
2026-05-26 14:36:30 +0900

    Add GA4 measurement tag

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -7,6 +7,7 @@ import { unescapeHTML } from "../util/escape"
 import { CustomOgImagesEmitterName } from "../../.quartz/plugins"
 
 const googleTagManagerId = "GTM-TP3TQ8KQ"
+const googleAnalyticsId = "G-23S1686YXS"
 
 export default (() => {
   const Head: QuartzComponent = ({
@@ -53,6 +54,15 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${googleTagManagerId}');`,
+          }}
+        />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', '${googleAnalyticsId}');`,
           }}
         />
         <title>{title}</title>
